a7d43f9998c2a44fff486dfdeefb2ef41fe07213
bannzai <kingkong999yhirose@gmail.com>
2017-11-20 00:22:26 +0900

    :up: resolved package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/jpsim/SourceKitten.git", majorVersion: 0, minor: 18),
         .Package(url: "https://github.com/vknabel/SwiftTryCatch.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/CommonCrypto.git", majorVersion: 0),
+        .Package(url: "https://github.com/bannzai/CommonCrypto/", majorVersion: 0),
         .Package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", majorVersion: 1),
         .Package(url: "https://github.com/tomlokhorst/XcodeEdit.git", majorVersion: 1)
     ]
